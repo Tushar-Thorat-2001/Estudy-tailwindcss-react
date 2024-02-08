301ad7dc9272f8fb9f91f2e76657886ca91a7756
Tushar-Thorat-2001 <thorat20187@gmail.com>
2024-02-09 01:27:58 +0530

    home component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 const App = () => {
   return (
-    <div><Navbar/></div>
+    <div><Navbar/>
+      
+      <Home/>
+    
+    </div>
   )
 }
 
